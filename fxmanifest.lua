fef7321a9bba7996447e1d4d651fda1ec3a928f3
savemeours <89605711+savemeours@users.noreply.github.com>
2025-05-31 11:29:57 +0700

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+game 'gta5'
+lua54 'yes'
+
+author 'savemeours'
+name 'Rockstar Editor based ox_lib'
+description 'Rockstar Editor based ox_lib'
+version '1.0.0'
+
+shared_script '@ox_lib/init.lua'
+
+client_scripts {
+    'client/*.lua'
+}
